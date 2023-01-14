86ae6772ac38edbe2785e06badef8dc54a65fe90
22HOP2e-bayra <HOP2-bayra@erins-iMac.local>
2023-01-14 14:04:48 +0800

    addedEverything

---
+++ backend/router/user.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  removeUser,
+  getUserByObject,
+} from "../controller/user.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllUsers);
+router.route("/:id").get(getUser).put(updateUser).delete(removeUser);
+router.route("/login").post(getUserByObject);
+router.route("/signup").post(createUser);
+
+export default router;
